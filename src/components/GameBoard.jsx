9e3d05a7b19fbfa60ce6d460fc97068571c8b0b9
Abraham Hdru <abraham.hdru@gmail.com>
2024-11-07 18:50:41 +0100

    Setting Active Player wirh props

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,7 +6,7 @@ const initialGameBoard = [
     [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ OnSelectSquare, activePlayerSymbol }) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex) {
@@ -14,9 +14,10 @@ export default function GameBoard() {
             const updatedBoard = [
                 ...prevGameBoard.map((innerArrray) => [...innerArrray]),
             ];
-            updatedBoard[rowIndex][colIndex] = "X";
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
         });
+        OnSelectSquare();
     }
     return (
         <ol id="game-board">
