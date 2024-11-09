3e00bf7556a36f216f79cea21598d321445bdb13
Abraham Hdru <abraham.hdru@gmail.com>
2024-11-09 12:58:29 +0100

    Changes to logging Turnchanges

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,19 +6,20 @@ const initialGameBoard = [
     [null, null, null],
 ];
 
-export default function GameBoard({ OnSelectSquare, activePlayerSymbol }) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ OnSelectSquare }) {
+    //  activePlayerSymbol;
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [
-                ...prevGameBoard.map((innerArrray) => [...innerArrray]),
-            ];
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
-        OnSelectSquare();
-    }
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedBoard = [
+    //             ...prevGameBoard.map((innerArrray) => [...innerArrray]),
+    //         ];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+    //     OnSelectSquare();
+    // }
     return (
         <ol id="game-board">
             {gameBoard.map((row, rowIndex) => (
@@ -26,11 +27,7 @@ export default function GameBoard({ OnSelectSquare, activePlayerSymbol }) {
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button
-                                    onClick={() =>
-                                        handleSelectSquare(rowIndex, colIndex)
-                                    }
-                                >
+                                <button onClick={OnSelectSquare}>
                                     {playerSymbol}
                                 </button>
                             </li>
